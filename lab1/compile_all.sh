a4cf5b4e46eba0136c69d3515e813f6e5b3d43ea
hsuwils1 <wilson.hsu@mail.utoronto.ca>
2019-01-10 15:04:19 -0500

    Completed Lab with no error messages shown

---
+++ lab1/compile_all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gcc -g -Wall -std=gnu99 -o hello hello.c
+gcc -g -Wall -std=gnu99 -o count count.c
+gcc -g -Wall -std=gnu99 -o echo_arg echo_arg.c 
+gcc -g -Wall -std=gnu99 -o echo_stdin echo_stdin.c
---
+++ lab1/my_commands.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./echo_arg csc209 > echo_out.txt
+./echo_stdin < echo_stdin.c
+./count 209 | wc -c
+ls -S -1 | ./echo_stdin 
