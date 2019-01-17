62a747fb5f3fab2afe14371bccb92f0d032af744
hsuwils1 <wilsonhsu0102@LAPTOP-LU8RDNVG.localdomain>
2019-01-17 15:58:44 -0500

    Changed the condition for while loop

--- lab2/phone_loop.c
+++ lab2/phone_loop.c
@@ -4,21 +4,25 @@ int main(int argc, char **argv) {
 	char phone[11];
 	int integer;
 	int error = 0;
+	int result;
 	scanf("%s", phone);
 	
-	while (1) {
-		scanf("%d", &integer);
-
-		if (integer == -1) {
+	while ((result = scanf("%d", &integer)) != EOF) {
+		if (result == 1){
+			if (integer == -1) {
 			printf("%s\n", phone);
+			}
+			else if (0 <= integer && integer <= 9) {
+				printf("%c\n", phone[integer]);
+			}
+			else {
+				printf("ERROR\n");
+				error = 1;
+			}
 		}
-		else if (0 <= integer && integer <= 9) {
-			printf("%c\n", phone[integer]);
-		}
-		else {
-			printf("ERROR\n");
-			error = 1;
-		}
+		else{
+			break;
+		}	
 	}
 	return error;
 }
