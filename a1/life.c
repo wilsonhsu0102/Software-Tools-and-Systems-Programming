b9285bfb659b52a4d866e2a9186eff18c3499ce2
hsuwils1 <wilson.hsu@mail.utoronto.ca>
2019-01-24 19:51:10 -0500

    created life's helper functions

--- a1/life.c
+++ a1/life.c
@@ -3,9 +3,34 @@
 #include <string.h>
 
 
-void print_state(char *state, int size);
-void update_state(char *state, int size);
+void print_state(char *state, int size) {
+    for (int i=0; i < size; i++) {
+	printf("%c", state[i]);
+    }
+    printf("\n");
+}
 
+void update_state(char *state, int size) {
+    char *new_state = malloc(size * sizeof(char));
+    if (size > 0) {
+	new_state[0] = state[0];
+    }
+    if (size > 1) {
+	new_state[size - 1] = state[size - 1];
+	for (int i=1; i < size - 1; i++) {
+	    if ((state[i - 1] == '.' && state[i + 1] == '.') || (state[i - 1] == 'X' && state[i + 1] == 'X')) {
+		new_state[i] = '.';
+	    } else {
+		new_state[i] = 'X';
+	    }
+        }
+    }
+    for (int i=0; i < size; i++) {
+	state[i] = new_state[i];
+    }
+    free(new_state);
+}
+    
 
 int main(int argc, char **argv) {
 
@@ -17,5 +42,11 @@ int main(int argc, char **argv) {
     int size = strlen(argv[1]);
     
     // TODO: complete the main function
-
+    int number_of_state = strtol(argv[2], NULL, 10);
+    char *state = argv[1];
+    print_state(state, size);
+    for (int i=0; i < number_of_state - 1; i++) {
+        update_state(state, size);
+	print_state(state, size);
+    }
 }
